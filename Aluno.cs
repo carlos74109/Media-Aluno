8cf315c8d258579f7a540d3891ffbd6907dec5c7
Carlos <carlos-84@live.com>
2022-01-25 15:42:16 -0300

    -------

---
+++ Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3.Models
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public Aluno(string nome, double nota1, double nota2, double nota3)
+        {
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.nota3 = nota3;
+
+           
+        }
+
+        public double mediaAluno()
+        {
+            return Math.Round((nota1 + nota2 + nota3) / 3, 1);
+        }
+
+        public void alunoAprovadoReprovado()
+        {
+            if(mediaAluno() >= 6)
+            {
+                Console.WriteLine("Aluno aprovado");
+            }else
+            {
+                double media = 6 - mediaAluno();
+                Console.WriteLine("Aluno reprovado, faltou " + media + " pontos");
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Exercicio3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("digite a nota da primeira unidade: ");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite a nota da segunda unidade: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite a nota da terceira unidade: ");
+            double nota3 = double.Parse(Console.ReadLine());
+
+
+            if (nota1 < 0 || nota2 < 0 || nota3 < 0)
+            {
+                Console.WriteLine("Digite notas validas");
+                return;
+            }
+
+            Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+
+            Console.WriteLine(aluno.mediaAluno());
+            aluno.alunoAprovadoReprovado();
+        }
+    }
+}
